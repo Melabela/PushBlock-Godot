3ecae7af7696bed7998009e1511a4ef9beb0461c
Melabela <43100514+Melabela@users.noreply.github.com>
2024-10-24 16:53:32 -0700

    PushBlock: change move from entire tile at once, to move sub-tiles over frames
    - move in partial steps to slide more smoothly, over MOVE_STEPS (set to 8 rn)

--- sprites/push_block.gd
+++ sprites/push_block.gd
@@ -1,41 +1,54 @@
 extends StaticBody2D
 
 const TILE_SIZE := 32.0
+const MOVE_STEPS := 8
 
-var current_move_dir := Vector2.ZERO
+var bIsMoving := false
+var nCurrStep := 0
+var move_per_step := Vector2.ZERO
 var target_position := Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+func clear_move_vars() -> void:
+	bIsMoving = false
+	nCurrStep = 0
+	move_per_step = Vector2.ZERO
+	target_position = Vector2.ZERO
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-## find position to closest TILE
-## (since block anchored to top-left, can just round in TILE-units)
-func get_nearest_tile_pos() -> Vector2:
-	var tile_pos := position / TILE_SIZE
-	return tile_pos.round() * TILE_SIZE
+func _physics_process(delta: float) -> void:
+	if bIsMoving:
+		nCurrStep += 1
+		var bColl := move_and_collide(move_per_step)
+		#print("PushBlock, isColl=", bColl != null)
+		## 1. if collision, shouldn't move off tile,
+		##     b/c wall tiles take entire tile space
+		## 2. if done steps, should be at target tile
+		if bColl or (nCurrStep >= MOVE_STEPS):
+			clear_move_vars()
 
 
 ## Returns if can accept the push_dir
-## Reasons it cannot are:
+## Reason it cannot:
 ##  - already moving in a direction
-##  - against a wall, and can't move in that dir
+## Collision is handled by physics engine, not detected in this fn
 func receive_push(push_dir: Vector2) -> bool:
-	if current_move_dir != Vector2.ZERO:
+	if bIsMoving:
 		#print("Receive_push(), return early, as already moving")
 		return false
 	
-	#current_move_dir = push_dir
-	target_position = position + (push_dir * TILE_SIZE)
+	var move := (push_dir * TILE_SIZE)
+	target_position = position + move
 	#print("Receive_push(), push_dir=", push_dir)
 	#print("    - position=", position, ", target_pos=", target_position)
 	
-	var coll := move_and_collide((push_dir * TILE_SIZE))
-	print("PushBlock, isColl=", coll != null)
+	move_per_step = move / MOVE_STEPS
+	nCurrStep = 0
+	bIsMoving = true
 	return true
