0ae21cca6d5ba731fb95bf1eb5dc9ced7735daf9
Melabela <43100514+Melabela@users.noreply.github.com>
2024-10-24 16:58:01 -0700

    PushBlock: fix odd collision behaviors at wall corners
    Issue: could not push outside red block, thru 1-tile wide tunnel bounded by walls
    
    Solution:  when being pushed, change the collision box
      - e.g. push left/right -> make box as left/right half, without top/bottom pixels
      - set this when start push, and set back to normal (box = full tile) when stopped

--- sprites/push_block.gd
+++ sprites/push_block.gd
@@ -1,12 +1,50 @@
 extends StaticBody2D
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
 const TILE_SIZE := 32.0
+const TILE_SIZE_HALF := TILE_SIZE / 2
+const TILE_SIZE_QRTR := TILE_SIZE / 4
 const MOVE_STEPS := 8
+
+const CollisionBoxes = {
+	Vector2.ZERO: {  ## i.e. not moving
+		## cover entire tile
+		"box_size": Vector2(TILE_SIZE, TILE_SIZE),
+		"center_pos": Vector2(TILE_SIZE_HALF, TILE_SIZE_HALF),
+	},
+	Vector2.LEFT: {
+		## cover left-half, except top & bottom pixels
+		"box_size": Vector2(TILE_SIZE_HALF, TILE_SIZE - 2.0),
+		"center_pos": Vector2(TILE_SIZE_QRTR, TILE_SIZE_HALF),
+	},
+	Vector2.RIGHT: {
+		## cover right-half, except top & bottom pixels
+		"box_size": Vector2(TILE_SIZE_HALF, TILE_SIZE - 2.0),
+		"center_pos": Vector2(3 * TILE_SIZE_QRTR, TILE_SIZE_HALF),
+	},
+	Vector2.UP: {
+		## cover top-half, except left & right pixels
+		"box_size": Vector2(TILE_SIZE - 2.0, TILE_SIZE_HALF),
+		"center_pos": Vector2(TILE_SIZE_HALF, TILE_SIZE_QRTR),
+	},
+	Vector2.DOWN: {
+		## cover bottom-half, except left & right pixels
+		"box_size": Vector2(TILE_SIZE - 2.0, TILE_SIZE_HALF),
+		"center_pos": Vector2(TILE_SIZE_HALF, 3 * TILE_SIZE_QRTR),
+	},
+}
 
 var bIsMoving := false
 var nCurrStep := 0
 var move_per_step := Vector2.ZERO
 var target_position := Vector2.ZERO
+
+
+func set_collision_box(move_dir: Vector2) -> void:
+	var box_info = CollisionBoxes[move_dir.normalized()]
+	(collision_shape_2d.shape as RectangleShape2D).size = box_info["box_size"]
+	collision_shape_2d.position = box_info["center_pos"]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +57,7 @@ func clear_move_vars() -> void:
 	nCurrStep = 0
 	move_per_step = Vector2.ZERO
 	target_position = Vector2.ZERO
+	set_collision_box(Vector2.ZERO)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -48,6 +87,7 @@ func receive_push(push_dir: Vector2) -> bool:
 	#print("Receive_push(), push_dir=", push_dir)
 	#print("    - position=", position, ", target_pos=", target_position)
 	
+	set_collision_box(push_dir)
 	move_per_step = move / MOVE_STEPS
 	nCurrStep = 0
 	bIsMoving = true
