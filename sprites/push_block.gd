dd9f5dabd7e65a5607cf64d4e3dc2a265b8c8c33
Melabela <43100514+Melabela@users.noreply.github.com>
2024-10-25 14:50:22 -0700

    PushBlock script: small changes
    - move slightly faster (8 frames -> 6)
    - remove unused global var 'target_position'
    - rename fn clean_move_vars() -> end_move()
    - rename fn var 'bColl' -> 'collision"
    - small cleanup & var rename in receive_push() fn

--- sprites/push_block.gd
+++ sprites/push_block.gd
@@ -3,12 +3,11 @@ extends StaticBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 const TILE_SIZE := 32.0
-const MOVE_STEPS := 8
+const MOVE_STEPS := 6
 
 var bIsMoving := false
 var nCurrStep := 0
 var move_per_step := Vector2.ZERO
-var target_position := Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,11 +15,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func clear_move_vars() -> void:
+func end_move() -> void:
 	bIsMoving = false
 	nCurrStep = 0
 	move_per_step = Vector2.ZERO
-	target_position = Vector2.ZERO
 	collision_shape_2d.set_collision_box(Vector2.ZERO)
 
 
@@ -28,13 +26,13 @@ func clear_move_vars() -> void:
 func _physics_process(delta: float) -> void:
 	if bIsMoving:
 		nCurrStep += 1
-		var bColl := move_and_collide(move_per_step)
+		var collision := move_and_collide(move_per_step)
 		#print("PushBlock, isColl=", bColl != null)
 		## 1. if collision, shouldn't move off tile,
 		##     b/c wall tiles take entire tile space
 		## 2. if done steps, should be at target tile
-		if bColl or (nCurrStep >= MOVE_STEPS):
-			clear_move_vars()
+		if collision or (nCurrStep >= MOVE_STEPS):
+			end_move()
 
 
 ## Returns if can accept the push_dir
@@ -46,13 +44,9 @@ func receive_push(push_dir: Vector2) -> bool:
 		#print("Receive_push(), return early, as already moving")
 		return false
 	
-	var move := (push_dir * TILE_SIZE)
-	target_position = position + move
-	#print("Receive_push(), push_dir=", push_dir)
-	#print("    - position=", position, ", target_pos=", target_position)
-	
 	collision_shape_2d.set_collision_box(push_dir)
-	move_per_step = move / MOVE_STEPS
+	var move_tile_size := push_dir * TILE_SIZE
+	move_per_step = move_tile_size / MOVE_STEPS
 	nCurrStep = 0
 	bIsMoving = true
 	return true
