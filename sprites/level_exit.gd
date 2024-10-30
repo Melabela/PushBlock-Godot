19d469b6ceea897bd012da8f046ae4fa2d90efbf
Melabela <43100514+Melabela@users.noreply.github.com>
2024-10-29 22:51:20 -0700

    Create LevelExit, LevelReset objects & scripts
    - add new Level(Exit/Reset) objects, with their sprites
    - add basic script to detect body entry, and only trigger if body is Player

---
+++ sprites/level_exit.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+signal level_exit_triggered
+
+const FRAME_DELAY := 6
+var frame_countdown := 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if frame_countdown > 0:
+		frame_countdown -= 1
+		if frame_countdown == 0:
+			level_exit_triggered.emit()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		#print("LevelExit: _on_body_entered(), Player")
+		frame_countdown = FRAME_DELAY
