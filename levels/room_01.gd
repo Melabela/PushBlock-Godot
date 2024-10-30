0a51d82dfd69f8c6d3c84f04bb67a2e4d162081e
Melabela <43100514+Melabela@users.noreply.github.com>
2024-10-29 23:11:48 -0700

    Room01: add 'LevelReset' & 'LevelExit' tiles, plus script framework
    - receive _on_level_(exit/reset)_triggered() signals in room_01.gd,
    - add fns to quit (exit) or reload the scene (reset)

--- levels/room_01.gd
+++ levels/room_01.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var viewport_area_bg: Polygon2D = $ViewportArea_Bg
 @onready var boundary_wall_tilemap_layer: TileMapLayer = $BoundaryWall_TilemapLayer
+@onready var level_reset: Area2D = $LevelTriggers/LevelReset
+@onready var level_exit: Area2D = $LevelTriggers/LevelExit
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +16,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_level_reset_triggered() -> void:
+	#print("_on_level_reset_triggered()")
+	get_tree().reload_current_scene()
+
+
+func _on_level_exit_triggered() -> void:
+	#print("_on_level_exit_triggered()")
+	get_tree().quit()
